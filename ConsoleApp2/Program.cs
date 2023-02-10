41b90421c350e9351dfa2c70eae4c1f19a909916
Hugge25 <111962627+KungeniDjunglen@users.noreply.github.com>
2023-02-10 10:30:02 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static bool PrimtalsUndersökarn(int tal)
+    {
+        for (int i = tal-1; 1 < i; i--)
+        {
+            if (tal % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static void Main()
+    {
+        Console.WriteLine("Skriv ett tal så undersöker vi om det är ett primtal");
+        int tal = int.Parse(Console.ReadLine());
+        if (PrimtalsUndersökarn(tal))
+        {
+            Console.WriteLine(tal + " är ett primtal");
+        } 
+        else
+        {
+            Console.WriteLine(tal + " är inte ett primtal");
+        }
+    }
+}
